55cbf9a5e8d3dbc05619dd95e37fae4b1e6198b8
mohammad192294 <mohammadalinew8@gmail.com>
2025-11-29 21:20:55 +0600

    Create background.js

---
+++ background.js
@@ -0,0 +1,6 @@
+// background.js
+
+// This file is required by Manifest V3 but does not need complex logic 
+// for simple content script injection.
+
+console.log("FB Focus Mode background service worker started.");
